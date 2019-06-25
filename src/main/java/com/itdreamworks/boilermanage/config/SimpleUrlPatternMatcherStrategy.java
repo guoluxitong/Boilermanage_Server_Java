dc0208eb6b7d92711529ad3a87af0a39cab81ca1
jialiang.li <80201288@qq.com>
2019-06-25 12:18:54 +0800

    清理合并后的cas配置

--- src/main/java/com/itdreamworks/boilermanage/config/SimpleUrlPatternMatcherStrategy.java
+++ src/main/java/com/itdreamworks/boilermanage/config/SimpleUrlPatternMatcherStrategy.java
@@ -1,6 +1,6 @@
 package com.itdreamworks.boilermanage.config;
 
-import org.jasig.cas.client.authentication.UrlPatternMatcherStrategy;
+//import org.jasig.cas.client.authentication.UrlPatternMatcherStrategy;
 
 import java.security.Policy;
 import java.util.regex.Pattern;
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
  * @Date: 2018/8/30 09:46
  * @Description:过滤掉不需要授权登录的页面
  */
-public class SimpleUrlPatternMatcherStrategy implements UrlPatternMatcherStrategy {
+public class SimpleUrlPatternMatcherStrategy {//implements UrlPatternMatcherStrategy {
     private Policy.Parameters parameters;
     private Pattern pattern;
     /**
@@ -21,19 +21,19 @@ public class SimpleUrlPatternMatcherStrategy implements UrlPatternMatcherStrateg
      * @return: true:不需要拦截
      *          false:必须要登录
      */
-    @Override
-    public boolean matches(String url) {
-        //使用正则表达式来匹配需要忽略的连接
-        return this.pattern.matcher(url).find();
-    }
-
-    /**
-     * description:正则表达式的规则，该规则在配置AuthenticationFilter的ignorePattern中设置
-     * @param: [pattern]
-     * @return: void
-     */
-    @Override
-    public void setPattern(String pattern) {
-        this.pattern = Pattern.compile(pattern);
-    }
+//    @Override
+//    public boolean matches(String url) {
+//        //使用正则表达式来匹配需要忽略的连接
+//        return this.pattern.matcher(url).find();
+//    }
+//
+//    /**
+//     * description:正则表达式的规则，该规则在配置AuthenticationFilter的ignorePattern中设置
+//     * @param: [pattern]
+//     * @return: void
+//     */
+//    @Override
+//    public void setPattern(String pattern) {
+//        this.pattern = Pattern.compile(pattern);
+//    }
 }
--- src/main/java/com/itdreamworks/boilermanage/web/TestController.java
+++ src/main/java/com/itdreamworks/boilermanage/web/TestController.java
@@ -4,10 +4,10 @@ import com.itdreamworks.boilermanage.entity.User;
 import com.itdreamworks.boilermanage.mapper.UserMapper;
 import com.itdreamworks.boilermanage.util.Result;
 import com.itdreamworks.boilermanage.util.ResultGenerator;
-import net.unicon.cas.client.configuration.EnableCasClient;
-import org.jasig.cas.client.authentication.AttributePrincipal;
-import org.jasig.cas.client.util.AbstractCasFilter;
-import org.jasig.cas.client.validation.Assertion;
+//import net.unicon.cas.client.configuration.EnableCasClient;
+//import org.jasig.cas.client.authentication.AttributePrincipal;
+//import org.jasig.cas.client.util.AbstractCasFilter;
+//import org.jasig.cas.client.validation.Assertion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.view.RedirectView;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.Map;
-@EnableCasClient
+//@EnableCasClient
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -29,53 +29,53 @@ public class TestController {
     @Autowired
     private UserMapper mapper;
 
-    @GetMapping("/signIn")
-    public Result signIn(HttpServletRequest request) {
-        //获取cas给我们传递回来的对象，这个对象在Session中
-        //session的 key是 CONST_CAS_ASSERTION
-        Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
+//    @GetMapping("/signIn")
+//    public Result signIn(HttpServletRequest request) {
+//        //获取cas给我们传递回来的对象，这个对象在Session中
+//        //session的 key是 CONST_CAS_ASSERTION
+//        Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
+//
+//        Principal principal = assertion.getPrincipal();
+//
+//        String loginName = principal.getName();
+//        System.out.printf("登录用户名:%s\r\n", loginName);
+//        User user = mapper.findOneByLoginId(loginName);
+//        System.out.printf("ValidFromDate:%s\r\n", assertion.getValidFromDate());
+//        System.out.printf("ValidUntilDate:%s\r\n", assertion.getValidUntilDate());
+//        System.out.printf("AuthenticationDate:%s\r\n", assertion.getAuthenticationDate());
+//
+//        //获取自定义返回值的数据
+//        if (principal instanceof AttributePrincipal) {
+//            //cas传递过来的数据
+//            Map<String, Object> result = ((AttributePrincipal) principal).getAttributes();
+//            for (Map.Entry<String, Object> entry : result.entrySet()) {
+//                String key = entry.getKey();
+//                Object val = entry.getValue();
+//                System.out.printf("%s:%s\r\n", key, val);
+//            }
+//        }
+//        return ResultGenerator.genSuccessResult(1, "success", user);
+//
+//
+//    }
 
-        Principal principal = assertion.getPrincipal();
-
-        String loginName = principal.getName();
-        System.out.printf("登录用户名:%s\r\n", loginName);
-        User user = mapper.findOneByLoginId(loginName);
-        System.out.printf("ValidFromDate:%s\r\n", assertion.getValidFromDate());
-        System.out.printf("ValidUntilDate:%s\r\n", assertion.getValidUntilDate());
-        System.out.printf("AuthenticationDate:%s\r\n", assertion.getAuthenticationDate());
-
-        //获取自定义返回值的数据
-        if (principal instanceof AttributePrincipal) {
-            //cas传递过来的数据
-            Map<String, Object> result = ((AttributePrincipal) principal).getAttributes();
-            for (Map.Entry<String, Object> entry : result.entrySet()) {
-                String key = entry.getKey();
-                Object val = entry.getValue();
-                System.out.printf("%s:%s\r\n", key, val);
-            }
-        }
-        return ResultGenerator.genSuccessResult(1, "success", user);
-
-
-    }
-
-    @GetMapping("/li")
-    public RedirectView li(HttpServletRequest request) {
-        Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
-
-        Principal principal = assertion.getPrincipal();
-
-        String loginName = principal.getName();
-
-        User user = mapper.findOneByLoginId(loginName);
-
-        //model.addAttribute(user);
-        RedirectView redirectView = new RedirectView("http://localhost:8081/");
-        //redirectView.addStaticAttribute("user",user);
-        System.out.println(redirectView);
-        return redirectView;
-        //return "redirect:http://localhost:8081/index.html";
-    }
+//    @GetMapping("/li")
+//    public RedirectView li(HttpServletRequest request) {
+//        Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
+//
+//        Principal principal = assertion.getPrincipal();
+//
+//        String loginName = principal.getName();
+//
+//        User user = mapper.findOneByLoginId(loginName);
+//
+//        //model.addAttribute(user);
+//        RedirectView redirectView = new RedirectView("http://localhost:8081/");
+//        //redirectView.addStaticAttribute("user",user);
+//        System.out.println(redirectView);
+//        return redirectView;
+//        //return "redirect:http://localhost:8081/index.html";
+//    }
 }
 
 
